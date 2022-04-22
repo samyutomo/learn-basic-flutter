8d0c8fa648a6ab4c5a2b1ae3661a5c615a089ac3
samyutomo <samyutomo@gmail.com>
2022-04-23 00:24:52 +0700

    learn example code 03: StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const Scaffold(
         body: Center(
-          child: Heading(text: "Hello world!"),
+          child: BiggerText(text: "Hello world!"),
         ),
       ),
     );
@@ -28,7 +28,40 @@ class Heading extends StatelessWidget {
   Widget build(BuildContext context) {
     return Text(
       text,
-      style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      style: const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+    );
+  }
+}
+
+class BiggerText extends StatefulWidget {
+  final String text;
+
+  const BiggerText({Key? key, required this.text}) : super(key: key);
+
+  @override
+  State<BiggerText> createState() => _BiggerTextState();
+}
+
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize = 16.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          widget.text,
+          style: TextStyle(fontSize: _textSize),
+        ),
+        ElevatedButton(
+            onPressed: () {
+              setState(() {
+                _textSize = 32.0;
+              });
+            },
+            child: const Text("Perbesar"))
+      ],
     );
   }
 }
