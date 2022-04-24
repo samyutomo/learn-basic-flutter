2ff9012157b376b11ee079de7366000b792095e4
samyutomo <samyutomo@gmail.com>
2022-04-24 07:03:38 +0700

    learn example code 04: Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -10,58 +10,41 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "Flutter Demo",
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const Scaffold(
-        body: Center(
-          child: BiggerText(text: "Hello world!"),
-        ),
+      home: const FirstScreen(),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.white,
+            )),
+        title: const Text("First Screen"),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+                color: Colors.white,
+              ))
+        ],
       ),
-    );
-  }
-}
-
-class Heading extends StatelessWidget {
-  final String text;
-
-  const Heading({Key? key, required this.text}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-    );
-  }
-}
-
-class BiggerText extends StatefulWidget {
-  final String text;
-
-  const BiggerText({Key? key, required this.text}) : super(key: key);
-
-  @override
-  State<BiggerText> createState() => _BiggerTextState();
-}
-
-class _BiggerTextState extends State<BiggerText> {
-  double _textSize = 16.0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          widget.text,
-          style: TextStyle(fontSize: _textSize),
-        ),
-        ElevatedButton(
-            onPressed: () {
-              setState(() {
-                _textSize = 32.0;
-              });
-            },
-            child: const Text("Perbesar"))
-      ],
+      body: const Center(
+        child: Text("Hello world!"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
