9ebd5796934b7654e16d32a1e0cf9b2a79097447
samyutomo <samyutomo@gmail.com>
2022-04-24 07:39:47 +0700

    learn example code 06: decoration - Container

--- lib/main.dart
+++ lib/main.dart
@@ -2,48 +2,54 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const FirstScreen());
 
-class FirstScreen extends StatelessWidget{
+class FirstScreen extends StatelessWidget {
   const FirstScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext  context) {
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Container",
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("First Screen"),
-        ),
-        body: Column(
-          children: [
-            Container(
-              child: const Text("Hi", style: TextStyle(fontSize: 40),),
-              color: Colors.blue,
-            ),
-            Container(
-              child: const Text("Hi", style: TextStyle(fontSize: 40),),
-              color: Colors.blue,
-              width: 200,
-              height: 100,
-            ),
-            Container(
-              child: const Text("Hi", style: TextStyle(fontSize: 40),),
-              color: Colors.blue,
-              padding: const EdgeInsets.all(10),
-            ),
-            Container(
-              child: const Text("Hi", style: TextStyle(fontSize: 40),),
-              color: Colors.blue,
-              margin: const EdgeInsets.all(10),
-            )
-          ],
-        ),
-      )
-
-
-    );
+        title: "Container",
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text("First Screen"),
+          ),
+          body: Column(
+            children: [
+              Container(
+                child: const Text("Hi", style: TextStyle(fontSize: 40),),
+                decoration: const BoxDecoration(color: Colors.red),
+              ),
+              Container(
+                child: const Text("Hi", style: TextStyle(fontSize: 40),),
+                decoration: const BoxDecoration(
+                    color: Colors.red,
+                    shape: BoxShape.circle),
+              ),
+              Container(
+                child: const Text("Hi", style: TextStyle(fontSize: 40),),
+                decoration: const BoxDecoration(
+                    color: Colors.red,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black,
+                          offset: Offset(3, 6),
+                          blurRadius: 10)
+                ]),
+              ),
+              Container(
+                  child: const Text("Hi", style: TextStyle(fontSize: 40),),
+                  decoration: BoxDecoration(
+                      color: Colors.red,
+                      border: Border.all(color: Colors.green, width: 3))),
+              Container(
+                  child: const Text("Hi", style: TextStyle(fontSize: 40),),
+                  decoration: BoxDecoration(
+                      color: Colors.red,
+                      border: Border.all(color: Colors.green, width: 3),
+                      borderRadius: BorderRadius.circular(10)))
+            ],
+          ),
+        ));
   }
-
 }
