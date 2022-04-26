68f6613d13e4f362afa9067d13ef29d6d266c4bb
samyutomo <samyutomo@gmail.com>
2022-04-26 08:28:55 +0700

    learn example code 13: ListView - builder

--- lib/main.dart
+++ lib/main.dart
@@ -22,20 +22,22 @@ class ScrollingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-          children: numberList.map((number) {
-        return Container(
-          height: 250,
-          decoration: BoxDecoration(
-              color: Colors.grey, border: Border.all(color: Colors.black)),
-          child: Center(
-            child: Text(
-              "$number",
-              style: const TextStyle(fontSize: 50),
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 250,
+            decoration: BoxDecoration(
+                color: Colors.grey, border: Border.all(color: Colors.black)),
+            child: Center(
+              child: Text(
+                "${numberList[index]}",
+                style: const TextStyle(fontSize: 50),
+              ),
             ),
-          ),
-        );
-      }).toList()),
+          );
+        },
+        itemCount: numberList.length,
+      ),
     );
   }
 }
