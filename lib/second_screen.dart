f3d5d1b2461e2ea140ce5ebb217320ac8525942a
samyutomo <samyutomo@gmail.com>
2022-04-26 14:07:07 +0700

    learn example code 16: Navigation - push & pop

---
+++ lib/second_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Second Screen"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text("Back to First Screen")),
+      ),
+    );
+  }
+}
