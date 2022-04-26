dee1383e36bdbe03bc578408ad500fad26de0125
samyutomo <samyutomo@gmail.com>
2022-04-26 09:07:28 +0700

    learn example code 15: Expanded - Flexible

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,26 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "ListView",
       theme: ThemeData(),
-      home: const ExpandedWidget(),
+      home: const ExpandedFlexibleWidget(),
+    );
+  }
+}
+
+class ExpandedFlexibleWidget extends StatelessWidget {
+  const ExpandedFlexibleWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [
+          Row(children: const [ExpandedWidget(), FlexibleWidget()]),
+          Row(children: const [ExpandedWidget(), ExpandedWidget()]),
+          Row(children: const [FlexibleWidget(), FlexibleWidget()]),
+          Row(children: const [FlexibleWidget(), ExpandedWidget()])
+        ],
+      )),
     );
   }
 }
@@ -20,38 +39,36 @@ class ExpandedWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-            child: Container(
-          color: Colors.red,
-        )),
-        Expanded(
-            child: Container(
-          color: Colors.orange,
-        )),
-        Expanded(
-            child: Container(
-          color: Colors.yellow,
-        )),
-        Expanded(
-            child: Container(
-          color: Colors.green,
-        )),
-        Expanded(
-            flex: 2,
-            child: Container(
-              color: Colors.blue,
-            )),
-        Expanded(
-            child: Container(
-          color: Colors.indigo,
-        )),
-        Expanded(
-            child: Container(
-          color: Colors.purple,
-        ))
-      ],
-    );
+    return Expanded(
+        child: Container(
+      decoration: BoxDecoration(
+          color: Colors.teal, border: Border.all(color: Colors.white)),
+      child: const Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Text(
+            "Expanded",
+            style: TextStyle(fontSize: 24, color: Colors.white),
+          )),
+    ));
+  }
+}
+
+class FlexibleWidget extends StatelessWidget {
+  const FlexibleWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+        child: Container(
+      decoration: BoxDecoration(
+          color: Colors.tealAccent, border: Border.all(color: Colors.white)),
+      child: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text(
+          "Flexible",
+          style: TextStyle(fontSize: 24, color: Colors.teal),
+        ),
+      ),
+    ));
   }
 }
