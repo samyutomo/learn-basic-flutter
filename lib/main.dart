40b71a47ec379d8cb2f708c6a0a863ac42833d26
samyutomo <samyutomo@gmail.com>
2022-04-26 08:16:15 +0700

    learn example code 13: ListView - dynamic

--- lib/main.dart
+++ lib/main.dart
@@ -10,65 +10,32 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "ListView",
       theme: ThemeData(),
-      home: const ScrollingScreen(),
+      home: ScrollingScreen(),
     );
   }
 }
 
 class ScrollingScreen extends StatelessWidget {
-  const ScrollingScreen({Key? key}) : super(key: key);
+  ScrollingScreen({Key? key}) : super(key: key);
+  final List<int> numberList = <int>[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-        children: [
-          Container(
-            height: 250,
-            decoration: BoxDecoration(
-                color: Colors.grey, border: Border.all(color: Colors.black)),
-            child: const Center(
-              child: Text(
-                "1",
-                style: TextStyle(fontSize: 50),
-              ),
+          children: numberList.map((number) {
+        return Container(
+          height: 250,
+          decoration: BoxDecoration(
+              color: Colors.grey, border: Border.all(color: Colors.black)),
+          child: Center(
+            child: Text(
+              "$number",
+              style: const TextStyle(fontSize: 50),
             ),
           ),
-          Container(
-            height: 250,
-            decoration: BoxDecoration(
-                color: Colors.grey, border: Border.all(color: Colors.black)),
-            child: const Center(
-              child: Text(
-                "2",
-                style: TextStyle(fontSize: 50),
-              ),
-            ),
-          ),
-          Container(
-            height: 250,
-            decoration: BoxDecoration(
-                color: Colors.grey, border: Border.all(color: Colors.black)),
-            child: const Center(
-              child: Text(
-                "3",
-                style: TextStyle(fontSize: 50),
-              ),
-            ),
-          ),
-          Container(
-            height: 250,
-            decoration: BoxDecoration(
-                color: Colors.grey, border: Border.all(color: Colors.black)),
-            child: const Center(
-              child: Text(
-                "4",
-                style: TextStyle(fontSize: 50),
-              ),
-            ),
-          )
-        ],
-      ),
+        );
+      }).toList()),
     );
   }
 }
