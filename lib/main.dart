8032ed355a32815401e9093c8a7e2e0263dcd473
samyutomo <samyutomo@gmail.com>
2022-04-26 17:42:27 +0700

    learn example code 17: Responsive Layout - LayoutBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -12,67 +12,46 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "Responsive Layout - MediaQuery vs LayoutBuilder",
       theme: ThemeData(),
-      home: const HomePage(),
+      home: const ResponsivePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class ResponsivePage extends StatelessWidget {
+  const ResponsivePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Size screenSize = MediaQuery.of(context).size;
-
     return Scaffold(
-        backgroundColor: Colors.blueGrey,
-        body: Row(
-          children: [
-            Expanded(child: LayoutBuilder(
-                builder: (BuildContext context, BoxConstraints constraint) {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    "MediaQuery: ${screenSize.width.toStringAsFixed(2)}",
-                    style: const TextStyle(color: Colors.white, fontSize: 18),
-                    textAlign: TextAlign.center,
-                  ),
-                  Text(
-                    "LayoutBuilder: ${constraint.maxWidth}",
-                    style: const TextStyle(color: Colors.white, fontSize: 18),
-                    textAlign: TextAlign.center,
-                  )
-                ],
-              );
-            })),
-            Expanded(
-                flex: 3,
-                child: LayoutBuilder(
-                    builder: (BuildContext context, BoxConstraints constraint) {
-                  return Container(
-                      color: Colors.white,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Text(
-                            "MediaQuery: ${screenSize.width.toStringAsFixed(2)}",
-                            style: const TextStyle(
-                                color: Colors.blueGrey, fontSize: 18),
-                            textAlign: TextAlign.center,
-                          ),
-                          Text(
-                            "LayoutBuilder: ${constraint.maxWidth}",
-                            style: const TextStyle(
-                                color: Colors.blueGrey, fontSize: 18),
-                            textAlign: TextAlign.center,
-                          )
-                        ],
-                      ));
-                }))
-          ],
-        ));
+      appBar: AppBar(),
+      body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constrain) {
+        if (constrain.maxWidth < 600) {
+          return ListView(
+            children: _generateContainers(),
+          );
+        } else if (constrain.maxWidth < 900) {
+          return GridView.count(
+            crossAxisCount: 2,
+            children: _generateContainers(),
+          );
+        } else {
+          return GridView.count(
+            crossAxisCount: 6,
+            children: _generateContainers(),
+          );
+        }
+      }),
+    );
+  }
+
+  List<Widget> _generateContainers() {
+    return List<Widget>.generate(20, (index) {
+      return Container(
+        margin: const EdgeInsets.all(8.0),
+        color: Colors.blueGrey,
+        height: 200,
+      );
+    });
   }
 }
