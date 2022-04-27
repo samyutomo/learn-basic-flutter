bee1e28b62f2a8011461f45a05ba37a79d052213
samyutomo <samyutomo@gmail.com>
2022-04-27 18:44:56 +0700

    learn example codelab 03 - complete the view with data from TourismPlace and add Hero animation

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,104 +1,96 @@
+import 'package:basic_flutter_dicoding/model/tourism_place.dart';
 import 'package:flutter/material.dart';
 
 class DetailScreen extends StatelessWidget {
-  const DetailScreen({Key? key}) : super(key: key);
+  final TourismPlace places;
+  const DetailScreen({Key? key, required this.places}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     var informationTextStyle = const TextStyle(fontFamily: 'Oxygen');
+
     return Scaffold(
         body: SafeArea(
-      child: SingleChildScrollView(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Hero(tag: "farmhouse", child: Image.asset('images/farm-house.jpg')),
-          Container(
-            margin: const EdgeInsets.only(top: 16.0),
-            child: const Text(
-              "Farm House Lembang",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 30.0, fontFamily: 'Staatliches'),
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(vertical: 16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
+          child: SingleChildScrollView(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Hero(tag: places.tag, child: Image.asset(places.imageAsset)),
+              Container(
+                margin: const EdgeInsets.only(top: 16.0),
+                child: Text(
+                  places.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                      fontSize: 30.0, fontFamily: 'Staatliches'),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    const Icon(Icons.calendar_today),
-                    const SizedBox(
-                      height: 8.0,
+                    Column(
+                      children: [
+                        const Icon(Icons.calendar_today),
+                        const SizedBox(
+                          height: 8.0,
+                        ),
+                        Text(
+                          places.openDays,
+                          style: informationTextStyle,
+                        )
+                      ],
                     ),
-                    Text(
-                      "Open Everyday",
-                      style: informationTextStyle,
+                    Column(
+                      children: [
+                        const Icon(Icons.access_time),
+                        const SizedBox(
+                          height: 8.0,
+                        ),
+                        Text(
+                          places.openTime,
+                          style: informationTextStyle,
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        const Icon(Icons.monetization_on),
+                        const SizedBox(height: 8.0),
+                        Text(
+                          places.ticketPrice,
+                          style: informationTextStyle,
+                        )
+                      ],
                     )
                   ],
                 ),
-                Column(
-                  children: [
-                    const Icon(Icons.access_time),
-                    const SizedBox(
-                      height: 8.0,
-                    ),
-                    Text(
-                      "09:00 - 20:00",
-                      style: informationTextStyle,
-                    )
-                  ],
+              ),
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  places.description,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 16.0, fontFamily: 'Oxygen'),
                 ),
-                Column(
-                  children: [
-                    const Icon(Icons.monetization_on),
-                    const SizedBox(height: 8.0),
-                    Text(
-                      "Rp 25.000",
-                      style: informationTextStyle,
-                    )
-                  ],
-                )
-              ],
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(16.0),
-            child: const Text(
-              "Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 16.0, fontFamily: 'Oxygen'),
-            ),
-          ),
-          Container(
-              height: 150,
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                  Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: Image.network(
-                              'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'))),
-                  Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: Image.network(
-                              'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'))),
-                  Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: Image.network(
-                              'https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg')))
-                ],
-              ))
-        ],
-      )),
-    ));
+              ),
+              Container(
+                  height: 150,
+                  margin: const EdgeInsets.only(bottom: 32.0),
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: places.imageUrls.map((index) {
+                        return Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image.network(index)));
+                      }).toList())
+              )
+            ],
+          )),
+        ));
   }
 }
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:basic_flutter_dicoding/detail_screen.dart';
+import 'package:basic_flutter_dicoding/model/tourism_place.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
@@ -10,42 +11,49 @@ class MainScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Wisata Bandung"),
       ),
-      body: InkWell(
-          onTap: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (BuildContext context) {
-              return const DetailScreen();
-            }));
+      body: ListView.builder(
+          itemBuilder: (BuildContext context, int index){
+            final TourismPlace place = tourismPlaceList[index];
+
+            return InkWell(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (BuildContext context) {
+                        return DetailScreen(places: place);
+                      }));
+                },
+                child: Card(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                          flex: 1,
+                          child: Hero(tag: place.tag, child: Image.asset(place.imageAsset))),
+                      Expanded(
+                          flex: 2,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  place.name,
+                                  style: const TextStyle(fontSize: 16.0),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Text(place.location)
+                              ],
+                            ),
+                          ))
+                    ],
+                  ),
+                ));
           },
-          child: Card(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                    flex: 1,
-                    child: Hero(tag: "farmhouse", child: Image.asset('images/farm-house.jpg'))),
-                Expanded(
-                    flex: 2,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: const [
-                          Text(
-                            "Farm House Lembang",
-                            style: TextStyle(fontSize: 16.0),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text("Lembang")
-                        ],
-                      ),
-                    ))
-              ],
-            ),
-          )),
+        itemCount: tourismPlaceList.length,
+      ),
     );
   }
 }
